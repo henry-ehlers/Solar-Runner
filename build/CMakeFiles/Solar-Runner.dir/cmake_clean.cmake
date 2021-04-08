3a3c63e097e106b25f5c7994f270c6ecf39a6ee8
Henry Ehlers <henry.ward.ehlers@gmail.com>
2021-04-08 07:02:59 +0000

    first cmake and make completed. only small changes were made to the cmake.txt file and the main.cpp file.

---
+++ build/CMakeFiles/Solar-Runner.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solar-Runner.dir/src/main.cpp.o"
+  "CMakeFiles/Solar-Runner.dir/src/game.cpp.o"
+  "CMakeFiles/Solar-Runner.dir/src/controller.cpp.o"
+  "CMakeFiles/Solar-Runner.dir/src/renderer.cpp.o"
+  "CMakeFiles/Solar-Runner.dir/src/ship.cpp.o"
+  "Solar-Runner.pdb"
+  "Solar-Runner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solar-Runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
