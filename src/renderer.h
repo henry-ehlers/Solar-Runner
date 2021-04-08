67b0e373d5691e98f7aab27e991e144c776e6627
Henry Ehlers <henry.ward.ehlers@gmail.com>
2021-04-08 07:09:21 +0000

    added the basic renderer.h file functionility outlined in the xample code of Udacity's SNAKEGAME.

---
+++ src/renderer.h
@@ -0,0 +1,36 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "SDL.h"
+#include "ship.h"
+
+class Renderer {
+ 
+  
+ public:
+  
+  # Constructor
+  Renderer(const std::size_t screen_width, const std::size_t screen_height);
+  
+  # Desctructor
+  ~Renderer();
+
+  # Render the Ship (and meteors once implemented)
+  void Render(Ship const ship);
+  
+  # Update the score
+  void UpdateWindowTitle(int score, int fps);
+
+ private:
+  
+  # SDL
+  SDL_Window   *sdl_window;
+  SDL_Renderer *sdl_renderer;
+
+  # The Screen Height and Width to be rendered
+  const std::size_t screen_width;
+  const std::size_t screen_height;
+  
+};
+
+#endif
---
+++ src/ship.h
@@ -0,0 +1,13 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+class Ship {
+  
+public:
+  
+private:
+  
+  float x_pos;
+  float y_pos;
+
+}
